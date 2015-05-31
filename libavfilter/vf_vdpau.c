61de7b06771532691eab6d52ac913a500583909c
Himangi Saraogi <himangi774@gmail.com>
2015-06-01 00:29:29 +0530

    Check return value from vdp_device_create_x11

--- libavfilter/vf_vdpau.c
+++ libavfilter/vf_vdpau.c
@@ -60,6 +60,11 @@ static av_cold int init(AVFilterContext *ctx)
 
     vdp_st = vdp_device_create_x11(s->dpy, s->screen,
                                    &s->vdp_device, &s->vdp_get_proc_address);
+    if (vdp_st != VDP_STATUS_OK) {
+        av_log(NULL, loglevel, "VDPAU device creation on X11 display %s failed.\n",
+               display);
+        return AVERROR(EINVAL);
+    }
 
     return 0;
 }
